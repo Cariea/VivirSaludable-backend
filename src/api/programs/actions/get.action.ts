eb4dc8900feddb02ee54c5a97a31045f688ec5cc
Carmelo Naim <carmelonaim30@gmail.com>
2024-02-06 01:28:13 -0400

    paginated response on programs

--- src/api/programs/actions/get.action.ts
+++ src/api/programs/actions/get.action.ts
@@ -1,14 +1,29 @@
 import { Response, Request } from 'express'
 import { pool } from '../../../database'
-import {  STATUS } from '../../../utils/constants'
+import {  DEFAULT_PAGE, STATUS } from '../../../utils/constants'
 import { handleControllerError } from '../../../utils/responses/handle-controller-error'
 import camelizeObject from '../../../utils/camelize-object'
+import {
+	PaginateSettings,
+	paginatedItemsResponse
+} from '../../../utils/responses'
 
 export const getPrograms = async (
-	_req: Request,
+	req: Request,
 	res: Response
 ): Promise<Response> => {
 	try {
+		const { page = DEFAULT_PAGE.page, size = DEFAULT_PAGE.size } = req.query
+		let offset = (Number(page) - 1) * Number(size)
+		if (Number(page) < 1) {
+			offset = 0
+		}
+		const { rows: programs } = await pool.query({
+			text: `
+        SELECT COUNT(*) 
+          FROM programs
+      `
+		})
 		const { rows } = await pool.query({
 			text: `
         SELECT 
@@ -16,9 +31,17 @@ export const getPrograms = async (
           name, 
           description
         FROM programs
-      `
+        LIMIT $1
+        OFFSET $2
+      `,
+			values: [size, offset]
 		})
-		return res.status(STATUS.OK).json(camelizeObject(rows))
+		const pagination: PaginateSettings = {
+			total: Number(programs[0].count),
+			page: Number(page),
+			perPage: Number(size)
+		}
+		return paginatedItemsResponse(res, STATUS.OK, camelizeObject(rows) as Array<Record<string, any>>, pagination)
 	} catch (error: unknown) {
 		console.error(error)
 		return handleControllerError(error, res)
