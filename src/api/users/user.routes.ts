d24faa417923762125c14398afb0e6b5ad83e14b
Carmelo Naim <carmelonaim30@gmail.com>
2024-02-11 11:20:46 -0400

    fix: programId in pacients register and updated register endpoint

--- src/api/users/user.routes.ts
+++ src/api/users/user.routes.ts
@@ -1,17 +1,21 @@
 import { Router } from 'express'
 
+//Schemas
+import { UserSchema } from './user.schema'
 // Consts
 import { AdminRoles } from '../../utils/roles.enum'
 
 //Middlewares
 import { hasAuthorization } from '../../middlewares/auth'
+import { schemaGuard } from '../../middlewares/schema-guard'
 
 //Controllers
 import { getAllUsers } from './actions/get.action'
+import { signUp } from '../users/actions/register.action'
 
 const router = Router()
 
 //Routes
 router.get('/', hasAuthorization([AdminRoles.ASISTENT]),getAllUsers)
-
+router.post('/register',hasAuthorization([AdminRoles.ASISTENT]), schemaGuard(UserSchema), signUp)
 export default router
