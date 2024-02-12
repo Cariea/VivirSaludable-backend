8283b6216fad25e6ed3daf6e9f242ebb6401547b
Carmelo Naim <carmelonaim30@gmail.com>
2024-02-12 19:31:03 -0400

    chat

---
+++ src/services/chat.ts
@@ -0,0 +1,30 @@
+import { Server, Socket } from 'socket.io'
+import { DefaultEventsMap } from 'socket.io/dist/typed-events'
+import { addMessage } from '../api/messages/actions/add.action'
+
+
+
+
+export const chat = (io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>) => {
+	const userIdToSocket = new Map<string, string>()
+	const socketToUserId = new Map<string, string>()
+	io.on('connection', (socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>) => {
+		console.log(`Nuevo cliente conectado: ${socket.id}, Usuario: ${socket.handshake.query.userId}`)
+		socketToUserId.set(socket.id, socket.handshake.query.userId as string)
+		userIdToSocket.set(socket.handshake.query.userId as string, socket.id)
+		socket.on('disconnect', () => {
+			console.log(`Se desconecto: ${socketToUserId.get(socket.id)}`)
+			userIdToSocket.delete(socketToUserId.get(socket.id) as string)
+			socketToUserId.delete(socket.id)
+		})
+		socket.on('chat message', (message) => {
+			if (userIdToSocket.get(message.to)) {
+				addMessage(socketToUserId.get(socket.id) as string, message.to, message.text)
+				io.to(userIdToSocket.get(message.to) as string).emit('chat message', message)
+			} else {
+				addMessage(socketToUserId.get(socket.id) as string, message.to, message.text)
+				console.log('Mensaje enviado de: ', socketToUserId.get(socket.id), 'a:', message.to, 'no se encuentra conectado')
+			}
+		})
+	})
+}
