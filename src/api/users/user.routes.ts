5a9cb731c165b55fb3d015d9345ba9362cc0e489
Carmelo Naim <carmelonaim30@gmail.com>
2024-02-13 22:59:22 -0400

    getUserById

--- src/api/users/user.routes.ts
+++ src/api/users/user.routes.ts
@@ -13,11 +13,12 @@ import { schemaGuard } from '../../middlewares/schema-guard'
 import { getAllUsers } from './actions/get.action'
 import { signUp } from '../users/actions/register.action'
 import { updateUser } from './actions/update.action'
-
+import { getByUserId } from './actions/get-by-user-id.action'
 const router = Router()
 
 //Routes
 router.get('/', hasAuthorization([AdminRoles.ASISTENT]),getAllUsers)
+router.get('/:userId',hasAuthorization([AdminRoles.ASISTENT]), getByUserId)
 router.post('/register',hasAuthorization([AdminRoles.ASISTENT]), schemaGuard(UserSchema), signUp)
 router.put('/:userId',hasAuthorization([AdminRoles.ASISTENT]), updateUser)
 export default router
