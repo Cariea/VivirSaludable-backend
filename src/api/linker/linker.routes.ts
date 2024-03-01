cb9c31b23a1da0269d4f3698a3875fcb80a8ca05
Carmelo Naim <carmelonaim30@gmail.com>
2024-03-01 16:36:23 -0400

    feat: delete specialist pacient

--- src/api/linker/linker.routes.ts
+++ src/api/linker/linker.routes.ts
@@ -12,11 +12,13 @@ import { hasAuthorization } from '../../middlewares/auth'
 
 //Controllers
 import { addPacients } from './actions/add.action'
+import { deleteLinker } from './actions/delete.action'
 
 const router = Router()
 
 //Routes
 
 router.post('/add', hasAuthorization([AdminRoles.ASISTENT]), schemaGuard(AddPacientToSpecialistSchema), addPacients)
+router.delete('/delete', hasAuthorization([AdminRoles.ASISTENT]), schemaGuard(AddPacientToSpecialistSchema), deleteLinker)
 
 export default router
