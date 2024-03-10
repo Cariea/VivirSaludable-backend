5694df0a8895b9eb4ab53a6dfb8a7086a64665ce
Carmelo Naim <carmelonaim30@gmail.com>
2024-03-10 07:56:44 -0400

    feat: lastMessages on contacts query

--- src/api/users/actions/get-contacts.ts
+++ src/api/users/actions/get-contacts.ts
@@ -1,10 +1,6 @@
 import { Response } from 'express'
 import { pool } from '../../../database'
-import { DEFAULT_PAGE, STATUS } from '../../../utils/constants'
-import {
-	PaginateSettings,
-	paginatedItemsResponse
-} from '../../../utils/responses'
+import {  STATUS } from '../../../utils/constants'
 import { handleControllerError } from '../../../utils/responses/handle-controller-error'
 import camelizeObject from '../../../utils/camelize-object'
 import { ExtendedRequest } from '../../../middlewares/auth'
@@ -13,88 +9,88 @@ export const getContacts = async (
 	req: ExtendedRequest,
 	res: Response
 ): Promise<Response> => {
-	const { page = DEFAULT_PAGE.page, size = DEFAULT_PAGE.size } = req.query
 	let response
-	let count
+
 	try {
-		let offset = (Number(page) - 1) * Number(size)
-		if (Number(page) < 1) {
-			offset = 0
-		}
+
 		if(req.user?.role === 'pacient') {
-			const { rows } = await pool.query({
-				text: `
-          SELECT COUNT(*) 
-            FROM assings
-            WHERE 
-            pacient_id = $1 AND assigned_status = true
-        `,
-				values: [req.user?.id]
-			})
-			count = rows[0].count
+
 			const { rows: contacts } = await pool.query({
 				text: `
-          SELECT 
-              s.user_id AS userId,
-              s.name AS name,
-              sp.name AS specialty
+            SELECT 
+            s.user_id AS userId,
+            s.name AS name,
+            sp.name AS specialty,
+            m.message AS last_message,
+            m.user_id AS last_message_sender_id,
+            m.created_at AS last_message_date
           FROM 
-              assings a
+            assings a
           JOIN 
-              specialists s ON a.specialist_id = s.user_id
+            specialists s ON a.specialist_id = s.user_id
           JOIN 
-              specialties sp ON s.speciality_id = sp.specialty_id
+            specialties sp ON s.speciality_id = sp.specialty_id
+          LEFT JOIN (
+            SELECT 
+              user_id,
+              user_receptor,
+              MAX(message_id) AS last_message_id
+            FROM 
+              messages
+            GROUP BY 
+              user_id, user_receptor
+          ) max_messages ON a.pacient_id = max_messages.user_id AND s.user_id = max_messages.user_receptor
+          LEFT JOIN messages m ON max_messages.user_id = m.user_id AND max_messages.last_message_id = m.message_id
           WHERE 
               a.pacient_id = $1 AND a.assigned_status = true
-          LIMIT $2
-          OFFSET $3
         `,
-				values: [req.user?.id, size, offset]
+				values: [req.user?.id ]
 			})
 			response = contacts
 		} else {
-			const { rows } = await pool.query({
-				text: `
-          SELECT COUNT(*) 
-            FROM assings
-            WHERE 
-            specialist_id = $1 AND assigned_status = true
-        `,
-				values: [req.user?.id]
-			})
-			count = rows[0].count
 			const { rows: contacts } = await pool.query({
 				text: `
           SELECT 
-              p.user_id AS userId,
-              p.name AS name,
-              pr.name AS program
+            p.user_id AS userId,
+            p.name AS name,
+            pr.name AS program,
+            m.message AS last_message,
+            m.user_id AS last_message_sender_id,
+            m.created_at AS last_message_date
           FROM 
-              assings a
+            assings a
           JOIN 
-              belongs b ON a.pacient_id = b.pacient_id AND a.asistent_id = b.asistent_id
+            belongs b ON a.pacient_id = b.pacient_id AND a.asistent_id = b.asistent_id
           JOIN 
-              pacients p ON a.pacient_id = p.user_id
+            pacients p ON a.pacient_id = p.user_id
           JOIN 
-              programs pr ON b.program_id = pr.program_id
+            programs pr ON b.program_id = pr.program_id
+          LEFT JOIN LATERAL (
+            SELECT 
+              message,
+              user_id,
+              created_at
+            FROM 
+              messages
+            WHERE 
+              (user_id = p.user_id AND user_receptor = $1) OR
+              (user_id = $1 AND user_receptor = p.user_id)
+            ORDER BY 
+              created_at DESC
+            LIMIT 1
+          ) m ON true
           WHERE 
-              a.specialist_id = $1 AND a.assigned_status = true     
-          LIMIT $2
-          OFFSET $3
+            a.specialist_id = $1 AND a.assigned_status = true
         `,
-				values: [req.user?.id, size, offset]
+				values: [req.user?.id]
 			})
 			response = contacts
 		}
 
-		const pagination: PaginateSettings = {
-			total: Number(count),
-			page: Number(page),
-			perPage: Number(size)
-		}
+
 		console.log('response', response) 
 
-		return paginatedItemsResponse(res, STATUS.OK, camelizeObject(response) as Array<Record<string, any>>, pagination)
+		return res.status(STATUS.OK).json(camelizeObject(response))
 	} catch (error: unknown) {
 		return handleControllerError(error, res)
 	}
